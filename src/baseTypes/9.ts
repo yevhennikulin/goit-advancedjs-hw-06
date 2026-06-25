84232202fccf82a2b2c266a2c1f67f9ab21f12af
Yevhen Nikulin <nikulinevg@gmail.com>
2026-06-25 18:28:32 +0300

    Complete HW 06

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,7 +2,20 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
+type PageDetails = {
+  createAt: Date;
+  updateAt: Date;
+};
+
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: 'open' | 'close';
+  details?: PageDetails;
+};
+
+const page1: Page = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -11,13 +24,13 @@ const page1 = {
     createAt: new Date('2021-01-01'),
     updateAt: new Date('2021-05-01'),
   }
-}
+};
 
-const page2 = {
+const page2: Page = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
-}
+};
 
 export {};
